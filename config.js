441b6af6c2edad29b32a58aad71ff8c7bf9deae0
angelocrisoseo <96187780+angelocrisoseo@users.noreply.github.com>
2022-12-24 16:34:18 +0800

    topush

---
+++ config.js
@@ -0,0 +1,8 @@
+const constants = {
+    openWeatherMap: {
+        BASE_URL: "https://api.openweathermap.org/data/2.5/weather?q=",
+        SECRET_KEY: "266650058525dacbdfe9f1b7d8e4f494"
+    }
+}
+
+module.exports = constants;
